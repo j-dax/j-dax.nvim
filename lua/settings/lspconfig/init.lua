179d957fabdb21100fe7e34e873f82977e62ca22
jdax <67487509+j-dax@users.noreply.github.com>
2024-09-08 09:49:46 -0700

    Registered tailwindcss with lspconfig, removed debug lines

--- lua/settings/lspconfig/init.lua
+++ lua/settings/lspconfig/init.lua
@@ -1,20 +1,17 @@
 local lspconfig = require 'lspconfig'
---
+
 -- Javascript / Typescript
 vim.g.markdown_fenced_languages = {
   'ts=typescript',
 }
 lspconfig.denols.setup {
-  on_attach = function()
-    print 'Deno loaded'
-  end,
   root_dir = lspconfig.util.root_pattern('deno.json', 'deno.jsonc'),
 }
 
 lspconfig.tsserver.setup {
-  on_attach = function()
-    print 'TSServer loaded'
-  end,
   root_dir = lspconfig.util.root_pattern 'package.json',
   single_file_support = false,
 }
+
+-- Tailwindcss
+lspconfig.tailwindcss.setup {}
